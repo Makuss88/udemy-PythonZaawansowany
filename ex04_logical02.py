9a2f12a2db08399524dba02543c351f03311f801
MakussBlue <g.maciborek@wp.pl>
2020-01-11 11:38:56 +0100

    Initial commit

---
+++ ex04_logical02.py
@@ -0,0 +1,15 @@
+import os
+
+def counterWord(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+        word_count = len(content.split())
+    return word_count
+
+
+path = r'txt04'
+if os.path.isfile(path):
+    print("How many words?? {}!".format(counterWord(path)))
+
+
+os.path.isfile(path) and print("How many words?? {}!".format(counterWord(path)))
