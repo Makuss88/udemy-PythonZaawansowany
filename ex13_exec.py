ae22a59f404eff89da633dd7c748f9a9c7a967bf
MakussBlue <g.maciborek@wp.pl>
2020-01-13 14:26:25 +0100

    ex13 - exec, you can give me many text to resolve, another class, bd, etc.

---
+++ ex13_exec.py
@@ -0,0 +1,14 @@
+import os
+files_to_process = [
+    r'C:\Users\Blu\projects\udemy\ex13a.py',
+    r'C:\Users\Blu\projects\udemy\ex13b.py'
+    ]
+
+counter = 0
+
+for file_path in files_to_process:
+    counter += 1
+    print('Process', counter)
+    with open(file_path, 'r') as f:
+        source = f.read()
+        exec(source)
